35b4c91bd927c704281b20e4ba546a41fd466bf8
Ivan I <blaerfdsfasfe@gmail.com>
2019-12-07 19:29:38 +0300

    1st commit

---
+++ my-function/lambda_function.py
@@ -0,0 +1,43 @@
+import json
+
+def lambda_handler(event, context):
+    ret = {}
+    ret['statusCode'] = 200
+    ret['headers'] = {
+        'Content-Type': 'text/html'
+    }
+
+    q_string = event["queryStringParameters"]
+    if ('show' in q_string):
+        body_str = f'''
+                <html>
+                <header><title>This is title</title></header>
+                <body>
+                Hello world Show %s <br>
+                and all is %s
+                </body>
+                </html>
+                    ''' % (q_string['show'], str(q_string)) 
+    else:
+        body_str = f'''
+            <html>
+                <header><title>This is title</title></header>
+            <body>
+                Hello world
+                <form action="./prod" method="get">
+                  First name: <input type="text" name="show"><br>
+                  <input type="checkbox" name="v1" value="Bike"> I have a bike<br>
+                  <input type="checkbox" name="v2" value="Car" checked> I have a car<br>
+                  Gender? <br>
+                  <input type="radio" name="gender" value="male" checked> Male<br>
+                  <input type="radio" name="gender" value="female"> Female<br>
+                  <input type="radio" name="gender" value="other"> Other<br><br>
+                  <input type="submit" value="Submit">
+                </form>
+            </body>
+            </html>
+                    '''
+
+    ret['body'] = body_str
+
+    return ret
---
+++ my-function/survey.py
@@ -0,0 +1,73 @@
+import yaml
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def parse_yaml_string(ys):
+    fd = StringIO(ys)
+    dct = yaml.load(fd, Loader=yaml.FullLoader)
+    return dct
+
+def get_html_survey_from_yaml_str(survey_str, form_action):
+    name_cnt = 1
+    html_str = f'''<form action="%s" method="get">''' % form_action
+    yaml_dict = parse_yaml_string(survey_str)
+    for item in yaml_dict:
+        if (item['type'] == 'desc'):
+            html_str += '\n' + item['desc'] + '<br>'
+        elif (item['type'] == 'radio'):
+            name = 'name' + str(name_cnt)
+            name_cnt += 1
+            opts = item['opts']
+            for opt in opts:
+                html_str += f'''
+<input type="%s" name="%s" value="%s"> %s <br> ''' % ('radio',name,opt['val'],opt['desc'])
+
+    html_str += '\n<input type="submit" value="Submit">'
+    html_str += "\n</form>"
+    return html_str
+
+def get_html_survey_res(var_val_map, survey_str):
+    name_cnt = 1
+    html_str = f'''<form action="" method="get">'''
+    yaml_dict = parse_yaml_string(survey_str)
+    for item in yaml_dict:
+        if (item['type'] == 'desc'):
+            html_str += '\n' + item['desc'] + '<br>'
+        elif (item['type'] == 'radio'):
+            name = 'name' + str(name_cnt)
+            name_cnt += 1
+            opts = item['opts']
+            for opt in opts:
+                checked = ''
+                if (var_val_map[name] == opt['val']):
+                    checked = 'checked'
+                html_str += f'''
+<input type="%s" name="%s" value="%s" %s> %s <br> ''' % ('radio',name,opt['val'],checked,opt['desc'])
+
+    html_str += "\n</form>"
+    return html_str
+
+
+ys = f'''
+- type: desc
+  desc: "gender?"
+- type: radio
+  opts:
+    - val: male
+      desc: "Male"
+    - val: female
+      desc: "Female"
+- type: desc
+  desc: "Car?"
+- type: radio
+  opts:
+    - val: bmw
+      desc: "BMW"
+    - val: mers
+      desc: "MERS"
+'''
+
+print(get_html_survey_from_yaml_str(ys, "./prod"))
+print(get_html_survey_res({'name1' : 'male', 'name2' : 'mers'}, ys))
